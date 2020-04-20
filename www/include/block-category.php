fb8c194566bb8e53f269181db59017db8c17c667
Kirill Sokolov <sokol0198@yandex.ru>
2020-04-21 01:39:03 +0700

    Completed block - car categories (smooth slide done)

--- www/include/block-category.php
+++ www/include/block-category.php
@@ -1,25 +1,46 @@
 <div id="block-category">
 <p class="header-title">Категории автомобилей</p>
     <ul>
-        <li><a>Легковые автомобили</a>
+        <li><a id="index1">Легковые автомобили</a>
             <ul class="category-section">
                 <li><a href=""><strong>Все автомобили</strong></a></li>
-                <li><a href="">Подраздел 1</a></li>
-                <li><a href="">Подраздел 1</a></li>
+                <?php
+                    $result = mysql_query("SELECT * FROM category_cars WHERE type_car='cars' ORDER BY mark_auto",$link);
+                        If (mysql_num_rows($result) > 0){
+                            $row = mysql_fetch_array($result);
+                            do{
+                                echo '<li><a href="view_cat.php?cat='.strtolower($row["mark_auto"]).'&type='.$row["type_car"].'">'.$row["mark_auto"].'</a></li>';
+                                }while ($row = mysql_fetch_array($result));
+                        } 
+                    ?>
             </ul>
         </li>
-        <li><a>Грузовые автомобили</a>
+        <li><a id="index2">Грузовые автомобили</a>
             <ul class="category-section">
                 <li><a href=""><strong>Все автомобили</strong></a></li>
-                <li><a href="">Подраздел 1</a></li>
-                <li><a href="">Подраздел 1</a></li>
+                <?php
+                    $result = mysql_query("SELECT * FROM category_cars WHERE type_car='trucks' ORDER BY mark_auto",$link);
+                        If (mysql_num_rows($result) > 0){
+                            $row = mysql_fetch_array($result);
+                            do{
+                                echo '<li><a href="view_cat.php?cat='.strtolower($row["mark_auto"]).'&type='.$row["type_car"].'">'.$row["mark_auto"].'</a></li>';
+                                }while ($row = mysql_fetch_array($result));
+                        } 
+                    ?>
             </ul>
         </li> 
-        <li><a>Пассажирские автомобили</a>
+        <li><a id="index3">Пассажирские автомобили</a>
             <ul class="category-section">
                 <li><a href=""><strong>Все автомобили</strong></a></li>
-                <li><a href="">Подраздел 1</a></li>
-                <li><a href="">Подраздел 1</a></li>
+                <?php
+                    $result = mysql_query("SELECT * FROM category_cars WHERE type_car='passenger' ORDER BY mark_auto",$link);
+                        If (mysql_num_rows($result) > 0){
+                            $row = mysql_fetch_array($result);
+                            do{
+                                echo '<li><a href="view_cat.php?cat='.strtolower($row["mark_auto"]).'&type='.$row["type_car"].'">'.$row["mark_auto"].'</a></li>';
+                                }while ($row = mysql_fetch_array($result));
+                        } 
+                    ?>
             </ul>
         </li>  
     </ul>
